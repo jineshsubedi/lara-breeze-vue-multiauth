c1f5b86b1332bc732017e63bd5dc50467f277101
jinesh subedi <jinesh1094@gmail.com>
2022-10-12 08:46:47 +0545

    initial commit

---
+++ app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Inertia\Inertia;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+class IsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(!Auth::check())
+        {
+            return Inertia::render('Auth/Login', [
+                'canResetPassword' => Route::has('password.request'),
+                'status' => session('status'),
+            ]);
+        }else{
+            if(Auth::user()->staff_type == 1)
+            {
+                return $next($request);
+            }else{
+                $request->session()->invalidate();
+                $request->session()->regenerateToken();
+                return redirect('/login');
+            }
+        }
+    }
+}
---
+++ resources/js/Pages/Supervisor/Dashboard.vue
@@ -0,0 +1,26 @@
+<script setup>
+import SupervisorLayout from '@/Layouts/SupervisorLayout.vue';
+import { Head } from '@inertiajs/inertia-vue3';
+</script>
+
+<template>
+    <Head title="Dashboard" />
+
+    <SupervisorLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Supervisor Dashboard
+            </h2>
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 bg-white border-b border-gray-200">
+                        You're logged in!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </SupervisorLayout>
+</template>
